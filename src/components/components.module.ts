e7a5d849b8ab9243ec6ce57d1aa0a8c9ce3395e2
CFE <hello@teamcfe.com>
2018-07-18 14:10:26 -0700

    8 - Ionic List Component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { StatusComponent } from './status/status';
 import { StatusDetailComponent } from './status-detail/status-detail';
 @NgModule({
 	declarations: [StatusComponent,
     StatusDetailComponent],
-	imports: [BrowserModule],
+	imports: [
+        BrowserModule,   
+        IonicModule
+    ],
 	exports: [StatusComponent,
     StatusDetailComponent]
 })
