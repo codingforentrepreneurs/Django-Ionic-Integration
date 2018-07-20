a5c5b130f26a9aa994bddd5dc5a218bf68754b45
CFE <hello@teamcfe.com>
2018-07-20 10:23:17 -0700

    21 - Loading Indicator

--- src/components/auth/auth.ts
+++ src/components/auth/auth.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {Validators, FormBuilder, FormGroup } from '@angular/forms';
-
+import { LoadingController } from 'ionic-angular'
 /**
  * Generated class for the AuthComponent component.
  *
@@ -13,20 +13,25 @@ import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 })
 export class AuthComponent {
     private userFormGroup: FormGroup;
+    loadingBar: any;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, 
+      public loadingCtrl: LoadingController) {
     console.log('Hello AuthComponent Component');
     this.userFormGroup = formBuilder.group({
         username: ['', Validators.required],
         password: ['', Validators.required]
 
     })
+    this.loadingBar = loadingCtrl.create({content: "Please wait..."})
   }
 
   handleSubmit(event) {
       event.preventDefault()
+      this.loadingBar.present()
       console.log(this.userFormGroup.value) // to the http server
       setTimeout(()=>{
+          this.loadingBar.dismiss()
           this.userFormGroup.reset()
       }, 2000)
   
