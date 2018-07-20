5f22d8a051faff1edf7956ead5a634c7fde75253
CFE <hello@teamcfe.com>
2018-07-20 13:59:39 -0700

    32 - HttpHeaders

--- src/providers/backend-api/backend-api.ts
+++ src/providers/backend-api/backend-api.ts
@@ -1,20 +1,37 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Storage } from '@ionic/storage';
 // observables - async
 
 const ROOT_ENDPOINT = 'http://127.0.0.1:8000/api/'
 @Injectable()
 export class BackendApiProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(public http: HttpClient, private storage: Storage) {
     console.log('Hello BackendApiProvider Provider');
+  }
+
+  getHttpOptions(includeAuth:boolean=true){
+      let myDefaultHeaders = {
+          'Content-Type': 'application/json'
+      }
+      this.storage.get('authToken').then((val)=>{
+          if (val && includeAuth){
+              myDefaultHeaders['Authorization'] = `JWT ${val}`
+          }
+      })
+      const httpOptions = {
+          headers: new HttpHeaders(myDefaultHeaders)
+      }
+
+      return httpOptions
   }
 
   login(userData:{}){
       console.log("working...")
       const endpoint = `${ROOT_ENDPOINT}auth/`
-      return this.http.post(endpoint, userData, {})
+      const options = this.getHttpOptions()
+      return this.http.post(endpoint, userData, options)
   }
   
 }
